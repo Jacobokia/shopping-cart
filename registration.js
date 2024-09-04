b49b4b09017cee8ff5002dcf1b38b99d158eb014
Jacobokia <cobs399@gmail.com>
2024-09-04 08:15:07 +0300

    create project files, login is working and storing to local storage

---
+++ registration.js
@@ -0,0 +1,38 @@
+let button = document.getElementById("btn")
+
+
+
+
+button.addEventListener('click', function (event) {
+    let firstName = document.getElementById("firstname").value
+let lastName = document.getElementById("firstname").value
+let phone = document.getElementById("phone").value
+let email = document.getElementById("email").value
+let password = document.getElementById("password").value
+let confirmPass= document.getElementById("confirmPass").value
+
+let userDetails ={
+    
+    firstName:firstName,
+    lastName:lastName,
+    phone:phone,
+    email:email,
+    password:password,
+    confirmPass:confirmPass
+
+}
+
+
+saveFormData(userDetails)
+alert("saved successfully")
+
+    
+})
+
+
+
+
+
+function saveFormData(formData) {
+    localStorage.setItem("userDetails", JSON.stringify(formData))
+}
